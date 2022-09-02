cd283a4e90b688b2fef63eaf9ce906b863e5b38e
Dan Banfield <dandbanfield@gmail.com>
2022-09-02 18:38:18 +0100

    Add application icon.

--- DR1 Coin Editor/MainForm.Designer.cs
+++ DR1 Coin Editor/MainForm.Designer.cs
@@ -28,14 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 307);
+            this.ClientSize = new System.Drawing.Size(580, 277);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
