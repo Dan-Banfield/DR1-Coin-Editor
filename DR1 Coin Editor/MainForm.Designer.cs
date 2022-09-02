90ee152ebc1ea4e88eebf0240d4d8ff1803a3e16
Dan Banfield <dandbanfield@gmail.com>
2022-09-02 17:59:43 +0100

    Create main form.

--- DR1 Coin Editor/MainForm.Designer.cs
+++ DR1 Coin Editor/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DR1_Coin_Editor
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(571, 307);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DR1 Coin Editor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- DR1 Coin Editor/MainForm.cs
+++ DR1 Coin Editor/MainForm.cs
@@ -1,8 +1,8 @@
 namespace DR1_Coin_Editor
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
